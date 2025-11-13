67edf5035ea0278d445f80294505bb9ee9946f82
SPPL23 <seanpatricklacsina@gmail.com>
2025-11-13 16:19:21 +0800

    First Comm

---
+++ personclass.py
@@ -0,0 +1,37 @@
+class MyPerson:
+    #sec = "3C\n"
+    #def pers_details():
+    #    name = "Sean"
+    #    age = "53"
+    #    motto = "The idle receives nothing, while the steadfast is imparted clarity and wisdom"
+#
+    #    print(f"name: {name}\nage: {age}\nmotto: {motto}\n")
+    #    return
+    
+    def __init__(self, sleep, peels):
+        print(f"Object Created {sleep} {peels}")
+
+#print(MyPerson.sec)
+#MyPerson.pers_details()
+#My = MyPerson
+MyPerson("MyPerson", "Sleepy")
+
+#class WoodWork:
+#    wood = "works"
+#
+#    def what_works(self):
+#        print(f"What? {self}\n")
+#
+#what = WoodWork
+#print(what.wood)
+#what.what_works("hey")
+
+class MainitPerson:
+    person = "mainit"
+    def Mainit():
+        print("Inet")
+        return
+    
+Mainit = MainitPerson
+
+Mainit.Mainit()
